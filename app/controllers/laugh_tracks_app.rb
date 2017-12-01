66b5ba0bd15bc3adf31780b764481972c7076974
TMad <madsen.tyler@gmail.com>
2017-12-01 11:01:30 -0700

    add laugh tracks through last iteration

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,13 @@
 class LaughTracksApp < Sinatra::Base
+  get '/comedians' do
+    @comedians = Comedian.all
+    @specials = Special.all
+    erb :comedians
+  end
+
+  get '/comedians' do
+    @comedians = Comedian.find_by(params[:age])
+    erb :comedians
+  end
+
 end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,22 @@
 class Comedian < ActiveRecord::Base
+  has_many :specials
 
+  def initialize(comedian_params)
+    @id          = comedian_params["id"] if comedian_params["id"]
+    @name        = comedian_params["name"]
+    @age         = comedian_params["age"]
+    @database    = SQLite3::Database.new('db/comedians_app_development.db')
+    @database.results_as_hash = true
+  end
+
+  def self.average_age
+    average(:age)
+  end
+
+  def self.find_by(age)
+    comedian = database.execute("SELECT * FROM comedians WHERE age=?", "#{age}")
+    comedian.map do |comedian|
+      Comedian.new(comedian)
+    end
+  end
 end
